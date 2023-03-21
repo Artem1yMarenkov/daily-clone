7e572ac289c5c08ca9afc56cf333f772e41df5ef
Artem1yMarenkov <orvysegor@gmail.com>
2023-03-21 11:11:24 +0300

    logo link added

--- src/widgets/Header/index.tsx
+++ src/widgets/Header/index.tsx
@@ -3,6 +3,8 @@ import { Button, Typography } from "@mui/material";
 import { Container, Stack } from "@mui/system";
 import { useStore } from "effector-react";
 import { $auth, logout } from "../../processes/auth";
+import { Link } from "atomic-router-react";
+import { homeRoute } from "../../entities/routes";
 
 const Header: FC = () => {
 	const { isLogin } = useStore($auth);
@@ -13,12 +15,14 @@ const Header: FC = () => {
 		<Stack sx={{ background: "white", padding: "15px", marginBottom: "70px" }}>
 			<Container>
 				<Stack direction="row" alignItems="center" justifyContent="space-between">
-					<Typography 
-						fontSize="20px" 
-						fontWeight="600"
-					>
-						школьный дневник
-					</Typography>
+					<Link to={homeRoute} href="/" style={{ textDecoration: "none", color: "black" }}>
+						<Typography 
+							fontSize="20px" 
+							fontWeight="600"
+						>
+							школьный дневник
+						</Typography>
+					</Link>
 					{
 						isLogin == true &&
 						<Button onClick={handleLogout} size="small">Выйти</Button>
